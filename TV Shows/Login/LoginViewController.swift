49d0e4b7ad282d9584b4403860c4062a666095f0
Erik Pahor <mutemuty1@gmail.com>
2022-07-11 16:27:11 +0200

    ui elements added

---
+++ TV Shows/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+    //
+//  LoginViewController.swift
+//  TV Shows
+//
+//  Created by Infinum on 11.07.2022..
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("test")
+    }
+    
+}
